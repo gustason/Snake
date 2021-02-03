15ff4bfbbdf5383c66cf82cc947d480f6604c0e5
gustason <gustas883@gmailcom>
2021-02-03 13:54:36 +0200

    Game in working state;

--- main.cpp
+++ main.cpp
@@ -6,24 +6,16 @@ using namespace std;
 #include <Windows.h>
 
 int nScreenWidth = 120;
-int nScreenHeight = 80;
+int nScreenHeight = 30;
 
 struct sSnakeSegment {
 	int x, y;
 };
 
-//enum eDirection {NORTH = 0, EAST, SOUTH, WEST};
-list<sSnakeSegment> snake = { {60, 15}, {61, 15}, {62, 15}, {63, 15}, {64, 15}, {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15} };
-int foodX = 30;
-int foodY = 15;
-int score = 0;
-bool isDead = false;
-//eDirection nSnakeDirection = WEST;
-int snakeDirection = 3;
-
 const int dx[] = { 0, 1, 0, -1 };
 const int dy[] = { -1, 0, 1, 0 };
 int main() {
+	srand(time(NULL));
 	// Create Screen Buffer. OneLoneCoder.com
 	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
 	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
@@ -31,56 +23,108 @@ int main() {
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
-	bool keyRight = false, keyLeft = false, keyRightOld = false, keyLeftOld = false;
 	while (1) {
-		// Timing & Input
-		this_thread::sleep_for(200ms);
+		//enum eDirection {NORTH = 0, EAST, SOUTH, WEST};
+		list<sSnakeSegment> snake = { {60, 15}, {61, 15}, {62, 15}, {63, 15}, {64, 15}, {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15} };
+		int foodX = 50;
+		int foodY = 15;
+		int score = 0;
+		bool isDead = false;
+		//eDirection nSnakeDirection = WEST;
+		int snakeDirection = 3;
+		bool keyRight = false, keyLeft = false, keyRightOld = false, keyLeftOld = false;
+		while (!isDead) {
+			// Timing & Input
+			//this::sleep_for(200ms);
 
-		// Get Keyboard Input
-        /// TO DO: Add all arrows.
-		keyRight = (0x8000 & GetAsyncKeyState((unsigned char)('\x27'))) != 0;
-		keyLeft = (0x8000 & GetAsyncKeyState((unsigned char)('\x25'))) != 0;
+			// Get Keyboard Input
+			// Adjust game delay based on player direction
+			auto t1 = chrono::system_clock::now();
+			while ((chrono::system_clock::now() - t1) < ((snakeDirection % 2) ? 120ms : 200ms)) {
+				/// TO DO: Add all arrows.
+				keyRight = (0x8000 & GetAsyncKeyState((unsigned char)('\x27'))) != 0;
+				keyLeft = (0x8000 & GetAsyncKeyState((unsigned char)('\x25'))) != 0;
 
-		if (keyRight && !keyRightOld) {
-			snakeDirection++;
-			if (snakeDirection == 4) snakeDirection = 0;
+				if (keyRight && !keyRightOld) {
+					snakeDirection++;
+					if (snakeDirection == 4) snakeDirection = 0;
+				}
+				if (keyLeft && !keyLeftOld) {
+					snakeDirection--;
+					if (snakeDirection == -1) snakeDirection = 3;
+				}
+
+				keyRightOld = keyRight;
+				keyLeftOld = keyLeft;
+			}
+
+			// Game Logic
+			// Update Snake Position
+			snake.push_front({ snake.front().x + dx[snakeDirection], snake.front().y + dy[snakeDirection] });
+
+			// Collision Detection
+			// Snake & Food
+			if (snake.front().x == foodX && snake.front().y == foodY)
+			{
+				score++;
+				while (screen[foodY * nScreenWidth + foodX] != L' ')
+				{
+					foodX = rand() % nScreenWidth;
+					foodY = (rand() % (nScreenHeight - 3)) + 3;
+				}
+
+				for (int i = 0; i < 5; i++)
+					snake.push_back({ snake.back().x, snake.back().y });
+			}
+			// Snake Against Itself
+			for (list<sSnakeSegment>::iterator it = snake.begin(); it != snake.end(); it++)
+				if (it != snake.begin() && (*it).x == snake.front().x && (*it).y == snake.front().y)
+					isDead = true;
+
+			// Snake & World
+			if (snake.front().x < 0 || snake.front().x >= nScreenWidth)
+				isDead = true;
+			if (snake.front().y < 3 || snake.front().y >= nScreenHeight)
+				isDead = true;
+
+			// Remove Tail
+			snake.pop_back();
+
+			// Display Frame
+			for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+
+			for (int i = 0; i < nScreenWidth; i++) {
+				screen[i] = L'=';
+				screen[2 * nScreenWidth + i] = L'=';
+			}
+
+			wsprintf(&screen[nScreenWidth + 5], L"S N A K E");
+			wsprintf(&screen[nScreenWidth + 50], L"SCORE: %d", score);
+			//wsprintf(&screen[nScreenWidth + 50], L"foodX: %d", foodX);
+			//wsprintf(&screen[nScreenWidth + 70], L"foodY: %d", foodY);
+
+
+
+			// Draw Snake Body
+			for (auto s : snake) {
+				screen[s.y * nScreenWidth + s.x] = isDead ? L'+' : L'~';
+			}
+			// Draw Snake Head
+			screen[snake.front().y * nScreenWidth + snake.front().x] = isDead ? L'x' : L'O';
+
+			// Draw Food
+			screen[foodY * nScreenWidth + foodX] = L'@';
+
+			// If Snake Died
+			if (isDead) {
+				wsprintf(&screen[15 * nScreenWidth + 40], L"     PRESS 'SPACE' TO PLAY AGAIN    ");
+			}
+
+			// Display Frame
+			WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 		}
-		if (keyLeft && !keyLeftOld) {
-			snakeDirection--;
-			if (snakeDirection == -1) snakeDirection = 3;
-		}
-
-		keyRightOld = keyRight;
-		keyLeftOld = keyLeft;
-
-		// Game Logic
-		// Update Snake Position
-		snake.push_front({ snake.front().x + dx[snakeDirection], snake.front().y + dy[snakeDirection] });
-
-		// Remove Tail
-		snake.pop_back();
-
-		// Display Frame
-		for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
-
-		for (int i = 0; i < nScreenWidth; i++) {
-			screen[i] = L'=';
-			screen[2 * nScreenWidth + i] = L'=';
-		}
-
-		wsprintf(&screen[nScreenWidth + 5], L"S N A K E");
-		wsprintf(&screen[nScreenWidth + 50], L"SCORE: %d", score);
-
-		// Draw Snake Body
-		for (auto s : snake) {
-			screen[s.y * nScreenWidth + s.x] = isDead ? L'+' : L'~';
-		}
-		// Draw Snake Head
-		screen[snake.front().y * nScreenWidth + snake.front().x] = isDead ? L'x' : L'O';
-
-		// Draw Food
-		screen[foodY * nScreenWidth + foodX] = L'@';
-		WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+		// Wait For User to Press Space
+		while ((0x8000 & GetAsyncKeyState((unsigned char)('\x20'))) == 0);
 	}
 	return 0;
 }
