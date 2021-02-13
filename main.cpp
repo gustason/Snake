ae2f9d775f57b4eb4f04f1e64d49693e7d0a0741
gustason <gustas883@gmailcom>
2021-02-13 22:42:51 +0200

    Added gamemodes, WASD support;

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// TODO: MAKE SNAKE COME OUT OF OTHER SIDE
 #include <iostream>
 #include <list>
 #include <thread>
@@ -11,9 +12,23 @@ int nScreenHeight = 30;
 struct sSnakeSegment {
 	int x, y;
 };
+enum eLayout {
+	CLASSIC = 0, MODERN
+};
 
 const int dx[] = { 0, 1, 0, -1 };
 const int dy[] = { -1, 0, 1, 0 };
+int highScore = 0;
+short gameMode = 0;
+
+int rand(int a, int b) {
+	return a + rand() % (b - a + 1);
+}
+bool isKeyPressed(char button) {
+	return (0x8000 & GetAsyncKeyState((unsigned char)(button))) != 0;
+}
+eLayout Layout = MODERN;
+
 int main() {
 	srand(time(NULL));
 	// Create Screen Buffer. OneLoneCoder.com
@@ -24,16 +39,26 @@ int main() {
 	DWORD dwBytesWritten = 0;
 
 	while (1) {
-		//enum eDirection {NORTH = 0, EAST, SOUTH, WEST};
 		list<sSnakeSegment> snake = { {60, 15}, {61, 15}, {62, 15}, {63, 15}, {64, 15}, {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15} };
 		int foodX = 50;
 		int foodY = 15;
 		int score = 0;
 		bool isDead = false;
-		//eDirection nSnakeDirection = WEST;
-		int snakeDirection = 3;
+		short snakeDirection = 3;
 		bool keyRight = false, keyLeft = false, keyUp = false, keyRightOld = false,
 			keyLeftOld = false, keyUpOld = false, keyDown = false, keyDownOld = false;
+
+		chrono::milliseconds tHorizontal, tVertical;
+		if (gameMode == 0 || gameMode == 2) { // NORMAL
+			tHorizontal = 115ms;
+			tVertical = 200ms;
+		}
+		else if (gameMode == 1) { // HARD
+			/// TODO: Do something more with this.
+			tHorizontal = 55ms;
+			tVertical = 110ms;
+		}
+
 		while (!isDead) {
 			// Timing & Input
 			//this::sleep_for(200ms);
@@ -41,34 +66,51 @@ int main() {
 			// Get Keyboard Input
 			// Adjust game delay based on player direction
 			auto t1 = chrono::system_clock::now();
-			while ((chrono::system_clock::now() - t1) < ((snakeDirection % 2) ? 120ms : 200ms)) {
-				/// TO DO: Add WASD support.
-				/// Make the player be able to choose between right and left and this layout.
-				keyRight = (0x8000 & GetAsyncKeyState((unsigned char)('\x27'))) != 0;
-				keyLeft = (0x8000 & GetAsyncKeyState((unsigned char)('\x25'))) != 0;
-				keyUp = (0x8000 & GetAsyncKeyState((unsigned char)('\x26'))) != 0;
-				keyDown = (0x8000 & GetAsyncKeyState((unsigned char)('\x28'))) != 0;
 
-				if (keyRight && !keyRightOld) {
-					if (snakeDirection == 0 || snakeDirection == 2) {
-						snakeDirection = 1;
+			while ((chrono::system_clock::now() - t1) < ((snakeDirection % 2) ? tHorizontal : tVertical)) {
+				/// TO DO: Add WASD support.
+				keyRight = isKeyPressed('\x27');
+				keyRight |= isKeyPressed('\x44');
+				keyLeft = isKeyPressed('\x25');
+				keyLeft |= isKeyPressed('\x41');
+				keyUp = isKeyPressed('\x26');
+				keyUp |= isKeyPressed('\x57');
+				keyDown = isKeyPressed('\x28');
+				keyDown |= isKeyPressed('\x53');
+
+				if (Layout == MODERN) {
+					if (keyRight && !keyRightOld) {
+						if (snakeDirection == 0 || snakeDirection == 2) {
+							snakeDirection = 1;
+						}
+					}
+					if (keyLeft && !keyLeftOld) {
+						if (snakeDirection == 0 || snakeDirection == 2) {
+							snakeDirection = 3;
+						}
+					}
+					if (keyUp && !keyUpOld) {
+						if (snakeDirection == 1 || snakeDirection == 3) {
+							snakeDirection = 0;
+						}
+					}
+					if (keyDown && !keyDownOld) {
+						if (snakeDirection == 1 || snakeDirection == 3) {
+							snakeDirection = 2;
+						}
 					}
 				}
-				if (keyLeft && !keyLeftOld) {
-					if (snakeDirection == 0 || snakeDirection == 2) {
-						snakeDirection = 3;
+				else if (Layout == CLASSIC) {
+					if (keyRight && !keyRightOld) {
+						snakeDirection++;
+						if (snakeDirection == 4) snakeDirection = 0;
+					}
+					if (keyLeft && !keyLeftOld) {
+						snakeDirection--;
+						if (snakeDirection == -1) snakeDirection = 3;
 					}
 				}
-				if (keyUp && !keyUpOld) {
-					if (snakeDirection == 1 || snakeDirection == 3) {
-						snakeDirection = 0;
-					}
-				}
-				if (keyDown && !keyDownOld) {
-					if (snakeDirection == 1 || snakeDirection == 3) {
-						snakeDirection = 2;
-					}
-				}
+
 
 				keyRightOld = keyRight;
 				keyLeftOld = keyLeft;
@@ -85,6 +127,12 @@ int main() {
 			if (snake.front().x == foodX && snake.front().y == foodY)
 			{
 				score++;
+				if (gameMode == 2) {
+					int randTime = rand(20, 130);
+					tHorizontal = (chrono::milliseconds)randTime;
+					randTime = rand(15, 180);
+					tVertical = (chrono::milliseconds)randTime;
+				}
 				while (screen[foodY * nScreenWidth + foodX] != L' ')
 				{
 					foodX = rand() % nScreenWidth;
@@ -116,13 +164,16 @@ int main() {
 				screen[2 * nScreenWidth + i] = L'=';
 			}
 
+			highScore = max(highScore, score);
 			wsprintf(&screen[nScreenWidth + 5], L"S N A K E");
-			//wsprintf(&screen[nScreenWidth + 50], L"SCORE: %d", score);
-			wsprintf(&screen[nScreenWidth + 50], L"keyUp: %d", keyUp);
-			wsprintf(&screen[nScreenWidth + 70], L"snakeDirection: %d", snakeDirection);
+			wsprintf(&screen[nScreenWidth + 50], L"SCORE: %d", score);
+			/// TODO: Add motivational qoutes if you reach a very high score (that would be pretty fun!)
+			wsprintf(&screen[nScreenWidth + 60], L"HI-SCORE: %d", highScore);
+
+			//wsprintf(&screen[nScreenWidth + 50], L"keyUp: %d", keyUp);
+			//wsprintf(&screen[nScreenWidth + 70], L"snakeDirection: %d", snakeDirection);
 			//wsprintf(&screen[nScreenWidth + 50], L"foodX: %d", foodX);
 			//wsprintf(&screen[nScreenWidth + 70], L"foodY: %d", foodY);
-
 
 
 			// Draw Snake Body
@@ -137,14 +188,73 @@ int main() {
 
 			// If Snake Died
 			if (isDead) {
-				wsprintf(&screen[15 * nScreenWidth + 40], L"     PRESS 'SPACE' TO PLAY AGAIN    ");
+				wsprintf(&screen[21 * nScreenWidth + 40], L"     PRESS 'SPACE' TO PLAY    ");
 			}
 
 			// Display Frame
 			WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 		}
+
+		// Show Gamemode Selection Screen
+		if (gameMode == 0) wsprintf(&screen[19 * nScreenWidth + 40], L"     -NORMAL- GAMEMODE SELECTED    ");
+		else if (gameMode == 1) wsprintf(&screen[19 * nScreenWidth + 40], L"     --HARD-- GAMEMODE SELECTED    ");
+		else if (gameMode == 2) wsprintf(&screen[19 * nScreenWidth + 40], L"     ---???--- GAMEMODE SELECTED    ");
+		wsprintf(&screen[20 * nScreenWidth + 40], L"     PRESS 'G' TO CHANGE GAMEMODE    ");
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
+		// Wait for User to press space
+		bool isSpacePressed = false;
+		while (!isSpacePressed) {
+			// Check if User wants to change Gamemode
+			bool isGPressed = false;
+			auto t1 = chrono::system_clock::now();
+			auto eps = 300ms;
+			while ((chrono::system_clock::now() - t1) < eps) {
+				isGPressed |= isKeyPressed('\x47');
+				isSpacePressed |= isKeyPressed('\x20');
+			}
+
+			// Change Gamemode and display frame
+			if (isGPressed) {
+				gameMode++;
+				if (gameMode == 3) gameMode = 0;
+				if (gameMode == 0) wsprintf(&screen[19 * nScreenWidth + 40], L"     -NORMAL- GAMEMODE SELECTED    ");
+				else if (gameMode == 1) wsprintf(&screen[19 * nScreenWidth + 40], L"     --HARD-- GAMEMODE SELECTED    ");
+				else if (gameMode == 2) wsprintf(&screen[19 * nScreenWidth + 40], L"     ---???--- GAMEMODE SELECTED    ");
+				wsprintf(&screen[20 * nScreenWidth + 40], L"     PRESS 'G' TO CHANGE GAMEMODE    ");
+				WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+			}
+		}
+		// TODO: Add Mayhem mode, where the speed of the snake changes pseudo-randomly with every food eaten.
+		// Show 1 Frame of Layout change menu
+		/*if (Layout == MODERN)
+			wsprintf(&screen[nScreenWidth + 80], L"PRESS 'L' TO CHANGE LAYOUT TO CLASSIC");
+		else if (Layout == CLASSIC)
+			wsprintf(&screen[nScreenWidth + 80], L"PRESS 'L' TO CHANGE LAYOUT TO MODERN");
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth* nScreenHeight, { 0,0 }, & dwBytesWritten);
+
 		// Wait For User to Press Space
-		while ((0x8000 & GetAsyncKeyState((unsigned char)('\x20'))) == 0);
+		for (int timePassed = 0; !isKeyPressed('\x20') ; timePassed++) {
+			bool keyLayout = (0x8000 & GetAsyncKeyState((unsigned char)('\x4C'))) != 0;
+			if (Layout == MODERN)
+				wsprintf(&screen[nScreenWidth + 80], L"PRESS 'L' TO CHANGE LAYOUT TO CLASSIC");
+			else if (Layout == CLASSIC)
+				wsprintf(&screen[nScreenWidth + 80], L"PRESS 'L' TO CHANGE LAYOUT TO MODERN");
+			if (keyLayout) {
+				switch (Layout) {
+				case MODERN:
+					Layout = CLASSIC;
+					break;
+				case CLASSIC:
+					Layout = MODERN;
+					break;
+				}
+			}
+
+			// TODO: Work with this..
+			if (keyLayout)
+				WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth* nScreenHeight, { 0,0 }, & dwBytesWritten);
+		}*/
 	}
 	return 0;
 }
